04c4e7d0237f0bbf5803cae2c83cd16d84e6a0a5
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-06-07 12:31:16 +0000

    header_update&seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 Admin.create!(
-    email: "nagano@cake",
-    password: "administrator"
-)# This file should contain all the record creation needed to seed the database with its default values.
+    email: "a@a",
+    password: "aaaaaa"
+)
+#  　 email: "nagano@cake",
+#     password: "administrator"
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
