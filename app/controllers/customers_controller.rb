15cf34ee1abf9e0f531678b69a3d16a1361f6d57
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-06-11 22:27:22 +0000

    customers_edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,21 @@ class CustomersController < ApplicationController
 
   def edit
   end
+  
+  def update
+    @end_user = current_end_user
+    if @end_user.update(end_user_params)
+      redirect_to mypage_path
+    else
+      render :edit
+    end
+  end
 
   def confirm
+  end
+  
+  private
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_kana_name, :first_kana_name, :postal_code, :address, :telephone_number, :password_confirmation)
   end
 end
