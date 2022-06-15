5bb978a37416499c947e630f069fde2390ad3f2f
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-06-15 02:27:44 +0000

    Update admin/genres_index_edit_link

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,7 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      redirect_to admin_genres_path, notice: 'Book was successfully created'
+      redirect_to admin_genres_path
     else
       @genres = Genre.all
       render :index
