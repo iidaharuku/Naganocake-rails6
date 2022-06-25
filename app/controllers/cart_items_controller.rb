b9609801884dfafb31e9afda911d17ffb8927d58
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-06-25 07:04:48 +0000

    Update cart_items_create

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,7 +2,14 @@ class CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.end_user_id = current_end_user.id
-    if @cart_item.save
+    
+    @item_id = CartItem.find_by(item_id: params[:cart_item][:item_id])
+    if @item_id == @cart_item.item_id
+      @cart_item_update = CartItem.find(@item_id)
+      amount = @cart_item_update.amount
+      @cart_item_update.amount = amount + @cart_item.amount
+      @cart_item_update.update(cart_item_params)
+    elsif @cart_item.save
       redirect_to cart_items_path
     else
       @item = Item.find(@cart_item.item_id)
