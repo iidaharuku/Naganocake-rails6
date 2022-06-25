3f9e59d7355dda0e1d37dcf1eb1b43e1e80fb7c9
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-06-25 08:35:38 +0000

    Fix cart/item#create amount==nil&&cart_item_id==still_present

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,8 +5,14 @@ class CartItemsController < ApplicationController
 
     @item_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
     if @item_item
-      @item_item.update(amount: @cart_item.amount + @item_item.amount)
-      redirect_to cart_items_path
+      if @cart_item.amount.present? == false
+        @item = Item.find(@cart_item.item_id)
+        @item_cost = @item.tax_free_cost*1.08.to_i
+        render template: "items/show"
+      else
+        @item_item.update(amount: @cart_item.amount + @item_item.amount)
+        redirect_to cart_items_path
+      end
     elsif @cart_item.save
       redirect_to cart_items_path
     else
