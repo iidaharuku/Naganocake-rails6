deae6b4964c7bff55c0b967069f8a9dee86d0f17
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-01 08:51:42 +0000

    Create address/create

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,22 @@ class AddressesController < ApplicationController
     @address = Address.new
     @addresses = Address.all
   end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.end_user_id = current_end_user.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
 
   def edit
+  end
+  
+  private
+  def address_params
+    params.require(:addresses).permit(:postal_code, :place, :name)
   end
 end
