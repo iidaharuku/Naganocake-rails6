980d2f60a17ac619c186befa0507b02bc49c573d
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-04 10:13:18 +0000

    Add orders_table

---
+++ db/migrate/20220704100645_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      
+      t.integer :end_user_id
+      t.string :postal_code
+      t.text :send_address
+      t.string :address_name
+      t.string :pay_way
+      t.integer :postage
+      t.integer :total_cost
+      t.integer :status
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_101609) do
+ActiveRecord::Schema.define(version: 2022_07_04_100645) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -101,6 +101,19 @@ ActiveRecord::Schema.define(version: 2022_06_29_101609) do
     t.text "explanation"
     t.integer "tax_free_cost"
     t.integer "stock_status", default: 1
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "end_user_id"
+    t.string "postal_code"
+    t.text "send_address"
+    t.string "address_name"
+    t.string "pay_way"
+    t.integer "postage"
+    t.integer "total_cost"
+    t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
