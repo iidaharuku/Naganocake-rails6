96796ed8bd9874754f7a5eb48be6f4340ea13438
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-04 10:56:38 +0000

    Add orders_routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get 'customers/confirm' => 'customers#confirm'
   patch 'customers/withdraw' => 'customers#withdraw'
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:new, :create, :index, :show]
+  get 'orders/complete' => 'orders#complete'
+  post 'orders' => 'orders#create'
+  post 'orders/confirm' => 'orders#con'
   devise_for :admin, controllers: {
     sessions: 'admin/sessions',
   }
