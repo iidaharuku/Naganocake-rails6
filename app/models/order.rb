b7b574b57c6fedfb138854d226d73fa569b04b84
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-04 10:48:04 +0000

    Add order_validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   
   belongs_to :end_user
+  
+  validates :end_user_id, presence: true
+  validates :postal_code, presence: true
+  validates :send_address, presence: true
+  validates :address_name, presence: true
+  validates :pay_way, presence: true
+  validates :postage, presence: true
+  validates :total_cost, presence: true
+  validates :status, presence: true ,default: 
+  
 end
