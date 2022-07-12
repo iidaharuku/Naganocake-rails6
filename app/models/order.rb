e2d77cbaf333b9ab9c58d4b57b6a9453c08f04e0
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-12 05:55:05 +0000

    Add order/new layout address/model address_display

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :end_user
-  
+
   validates :end_user_id, presence: true
   validates :postal_code, presence: true
   validates :send_address, presence: true
@@ -9,6 +9,8 @@ class Order < ApplicationRecord
   validates :pay_way, presence: true
   validates :postage, presence: true
   validates :total_cost, presence: true
-  validates :status, presence: true ,default: 
+  validates :status, presence: true ,default: 0
+
   
+
 end
