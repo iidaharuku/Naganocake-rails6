df32cc2e43a094e945782e66909b9198a155748e
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-13 08:11:26 +0000

    Fix order/confirm pay_way&postal_code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,31 +2,31 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def confirm
     @order = Order.new(order_params)
     @order.end_user_id = current_end_user.id
-    
+
     @cart_items = CartItem.where(end_user_id: current_end_user)
     @sum = 0
     @cart_items.each  do |cart_item|
       @sum += (cart_item.item.tax_free_cost*1.08).to_i * cart_item.amount
     end
-    @order.total_cost = @sum
-    if @order.postal_code == 0
+    if @order.total_cost == 0
       @order.postal_code = current_end_user.postal_code
       @order.send_address = current_end_user.address
       @order.address_name = current_end_user.last_name + current_end_user.first_name
-    elsif @order.postal_code == 1
+    elsif @order.total_cost == 1
       order_address = Address.find_by(id: postage)
       @order.postal_code = order_address.postal_code
       @order.send_address = order_address.place
       @order.address_name = order_address.name
     end
-      
+    @order.total_cost = @sum + 800
   end
+
   def complete
-    
+
   end
 
   def index
@@ -34,9 +34,9 @@ class OrdersController < ApplicationController
 
   def show
   end
-  
+
   private
   def order_params
-    params.require(:order).permit(:payway, :postal_code, :postage)
+    params.require(:order).permit(:pay_way, :postal_code, :postage, :send_address, :address_name, :total_cost)
   end
 end
