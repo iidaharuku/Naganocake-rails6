dcb56490ce4f1bc3635e33e047c8ea572dfcc715
Iida Haruku <iida.haruku.infratop@gmail.com>
2022-07-15 08:56:36 +0000

    Add admin/order_detail status_update

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @detail = OrderDetail.find(params[:id])
+    @detail.update(detail_params)
+    redirect_to admin_order_path(@detail.order_id)
+  end
+
+  private
+  def detail_params
+    params.require(:order_detail).permit(:create_status)
+  end
 end
